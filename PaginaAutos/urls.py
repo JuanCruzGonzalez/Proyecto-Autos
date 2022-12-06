2f6b9b6dc863487b25e4edcd99fad5e06621d9f6
Juan Cruz Gonzalez <juancgonzalez0901@gmail.com>
2022-12-06 20:58:55 -0300

    Comienzo de la estetica de la Web y uso de botones

--- PaginaAutos/urls.py
+++ PaginaAutos/urls.py
@@ -4,4 +4,9 @@ from PaginaAutos import views
 
 urlpatterns = [
     path('inicio/', Inicio, name='autos-inicio'),
+    path('modelos/', Autos, name='autos-modelos'),
+    path('marcas/', Marcas, name='autos-marcas'),
+    path('motores/', Motores, name='autos-motores'),
+    path('contacto/', Contacto, name='contacto'),
+    path('sobre-nosotros/', Nosotros, name='sobre-nosotros'),
  ]
--- PaginaAutos/views.py
+++ PaginaAutos/views.py
@@ -3,3 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 def Inicio(request):
     return render(request, 'PaginaAutos/index.html')
+
+def Autos(request):
+    return render(request, 'PaginaAutos/autos.html')
+
+def Marcas(request):
+    return render(request, 'PaginaAutos/marcas.html')
+
+def Motores(request):
+    return render(request, 'PaginaAutos/motores.html')
+
+def Nosotros(request):
+    return render(request, 'PaginaAutos/nosotros.html')
+
+def Contacto(request):
+    return render(request, 'PaginaAutos/contacto.html')
+
+
